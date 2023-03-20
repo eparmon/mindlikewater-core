8ba6b86c90a2fbe4dfdaa672897a181780a9e2e1
Gene <eparmon@tuta.io>
2023-03-20 15:47:20 +0400

    Initialize Spring Boot application with DB connection

---
+++ src/test/kotlin/app/mindlikewater/core/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.mindlikewater.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
