ea6ad663a1e3c2220ba03c1e31ba022e854c0fcc
Gene <eparmon@tuta.io>
2023-03-21 17:54:54 +0400

    Add created_at and deleted_at columns

--- src/main/kotlin/app/mindlikewater/core/persistence/domain/CapturedItem.kt
+++ src/main/kotlin/app/mindlikewater/core/persistence/domain/CapturedItem.kt
@@ -2,11 +2,14 @@ package app.mindlikewater.core.persistence.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
 
 @Table("captured_items")
 class CapturedItem(val accountId: Int, val text: String) {
 
     @Id
     var id: Int? = null
+
+    var deletedAt: LocalDateTime? = null
 
 }
